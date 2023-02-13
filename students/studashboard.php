d8107b2acee54c15631c2577a64060bf553883b7
Arnob <arnobm97@gmail.com>
2023-02-13 21:41:06 +0600

    dashboard added

---
+++ students/studashboard.php
@@ -0,0 +1,171 @@
+<?php
+    include "../includes/header.php";
+    include "../includes/navbar.php";
+    include "../includes/dbConfig.php";
+?>
+ <div class="btn-section">
+            <div class="username-btn"><button></button></div>
+                <a href="../index.php">
+                    <div class="logout-btn"><button>logout</button></div>
+                </a>
+            </div>
+<div class="course-wrapper">
+<h1 class="course-title-text">Our Offered Courses Right Now</h1>
+
+<div class="courses-card">
+    <div class="flip-card">
+            <div class="flip-card-inner">
+                <div class="flip-card-front">
+                    <img src="/coursereg/media/images/sample1.png" class="flip-card-img">
+                </div>
+                <div class="flip-card-back">
+                    <h1>Computer Security</h1> 
+                    <p>Teacher: Mr. Elon Musk</p> 
+                    <p>Course Starts on: 12 March, 2023</p>
+                    <p>Course Ends on: 12 July, 2023</p>
+                    <p>Course Fees: 4000 BDT</p>
+                    <abbr title="computer security, also called cybersecurity, the protection of computer systems and information from harm, theft, and unauthorized use. Computer hardware is typically protected by the same means used to protect other valuable or sensitive equipment—namely, serial numbers, doors and locks, and alarms. The protection of information and system access, on the other hand, is achieved through other tactics, some of them quite complex.">Details</abbr>
+                </div>
+            </div>
+        </div>
+        <div class="flip-card">
+            <div class="flip-card-inner">
+                <div class="flip-card-front">
+                    <img src="/coursereg/media/images/sample1.png" class="flip-card-img">
+                </div>
+                <div class="flip-card-back">
+                    <h1>Computer Security</h1> 
+                    <p>Teacher: Mr. Elon Musk</p> 
+                    <p>Course Starts on: 12 March, 2023</p>
+                    <p>Course Ends on: 12 July, 2023</p>
+                    <p>Course Fees: 4000 BDT</p>
+                    <abbr title="computer security, also called cybersecurity, the protection of computer systems and information from harm, theft, and unauthorized use. Computer hardware is typically protected by the same means used to protect other valuable or sensitive equipment—namely, serial numbers, doors and locks, and alarms. The protection of information and system access, on the other hand, is achieved through other tactics, some of them quite complex.">Details</abbr>
+                </div>
+            </div>
+        </div>
+        <div class="flip-card">
+            <div class="flip-card-inner">
+                <div class="flip-card-front">
+                    <img src="/coursereg/media/images/sample1.png" class="flip-card-img">
+                </div>
+                <div class="flip-card-back">
+                    <h1>Computer Security</h1> 
+                    <p>Teacher: Mr. Elon Musk</p> 
+                    <p>Course Starts on: 12 March, 2023</p>
+                    <p>Course Ends on: 12 July, 2023</p>
+                    <p>Course Fees: 4000 BDT</p>
+                    <abbr title="computer security, also called cybersecurity, the protection of computer systems and information from harm, theft, and unauthorized use. Computer hardware is typically protected by the same means used to protect other valuable or sensitive equipment—namely, serial numbers, doors and locks, and alarms. The protection of information and system access, on the other hand, is achieved through other tactics, some of them quite complex.">Details</abbr>
+                </div>
+            </div>
+        </div>
+        <div class="flip-card">
+            <div class="flip-card-inner">
+                <div class="flip-card-front">
+                    <img src="/coursereg/media/images/sample1.png" class="flip-card-img">
+                </div>
+                <div class="flip-card-back">
+                    <h1>Computer Security</h1> 
+                    <p>Teacher: Mr. Elon Musk</p> 
+                    <p>Course Starts on: 12 March, 2023</p>
+                    <p>Course Ends on: 12 July, 2023</p>
+                    <p>Course Fees: 4000 BDT</p>
+                    <abbr title="computer security, also called cybersecurity, the protection of computer systems and information from harm, theft, and unauthorized use. Computer hardware is typically protected by the same means used to protect other valuable or sensitive equipment—namely, serial numbers, doors and locks, and alarms. The protection of information and system access, on the other hand, is achieved through other tactics, some of them quite complex.">Details</abbr>
+                </div>
+            </div>
+        </div>
+        <div class="flip-card">
+            <div class="flip-card-inner">
+                <div class="flip-card-front">
+                    <img src="/coursereg/media/images/sample1.png" class="flip-card-img">
+                </div>
+                <div class="flip-card-back">
+                    <h1>Computer Security</h1> 
+                    <p>Teacher: Mr. Elon Musk</p> 
+                    <p>Course Starts on: 12 March, 2023</p>
+                    <p>Course Ends on: 12 July, 2023</p>
+                    <p>Course Fees: 4000 BDT</p>
+                    <abbr title="computer security, also called cybersecurity, the protection of computer systems and information from harm, theft, and unauthorized use. Computer hardware is typically protected by the same means used to protect other valuable or sensitive equipment—namely, serial numbers, doors and locks, and alarms. The protection of information and system access, on the other hand, is achieved through other tactics, some of them quite complex.">Details</abbr>
+                </div>
+            </div>
+        </div>
+        <div class="flip-card">
+            <div class="flip-card-inner">
+                <div class="flip-card-front">
+                    <img src="/coursereg/media/images/sample1.png" class="flip-card-img">
+                </div>
+                <div class="flip-card-back">
+                    <h1>Computer Security</h1> 
+                    <p>Teacher: Mr. Elon Musk</p> 
+                    <p>Course Starts on: 12 March, 2023</p>
+                    <p>Course Ends on: 12 July, 2023</p>
+                    <p>Course Fees: 4000 BDT</p>
+                    <abbr title="computer security, also called cybersecurity, the protection of computer systems and information from harm, theft, and unauthorized use. Computer hardware is typically protected by the same means used to protect other valuable or sensitive equipment—namely, serial numbers, doors and locks, and alarms. The protection of information and system access, on the other hand, is achieved through other tactics, some of them quite complex.">Details</abbr>
+                </div>
+            </div>
+        </div>
+        <div class="flip-card">
+            <div class="flip-card-inner">
+                <div class="flip-card-front">
+                    <img src="/coursereg/media/images/sample1.png" class="flip-card-img">
+                </div>
+                <div class="flip-card-back">
+                    <h1>Computer Security</h1> 
+                    <p>Teacher: Mr. Elon Musk</p> 
+                    <p>Course Starts on: 12 March, 2023</p>
+                    <p>Course Ends on: 12 July, 2023</p>
+                    <p>Course Fees: 4000 BDT</p>
+                    <abbr title="computer security, also called cybersecurity, the protection of computer systems and information from harm, theft, and unauthorized use. Computer hardware is typically protected by the same means used to protect other valuable or sensitive equipment—namely, serial numbers, doors and locks, and alarms. The protection of information and system access, on the other hand, is achieved through other tactics, some of them quite complex.">Details</abbr>
+                </div>
+            </div>
+        </div>
+        <div class="flip-card">
+            <div class="flip-card-inner">
+                <div class="flip-card-front">
+                    <img src="/coursereg/media/images/sample1.png" class="flip-card-img">
+                </div>
+                <div class="flip-card-back">
+                    <h1>Computer Security</h1> 
+                    <p>Teacher: Mr. Elon Musk</p> 
+                    <p>Course Starts on: 12 March, 2023</p>
+                    <p>Course Ends on: 12 July, 2023</p>
+                    <p>Course Fees: 4000 BDT</p>
+                    <abbr title="computer security, also called cybersecurity, the protection of computer systems and information from harm, theft, and unauthorized use. Computer hardware is typically protected by the same means used to protect other valuable or sensitive equipment—namely, serial numbers, doors and locks, and alarms. The protection of information and system access, on the other hand, is achieved through other tactics, some of them quite complex.">Details</abbr>
+                </div>
+            </div>
+        </div>
+        <div class="flip-card">
+            <div class="flip-card-inner">
+                <div class="flip-card-front">
+                    <img src="/coursereg/media/images/sample1.png" class="flip-card-img">
+                </div>
+                <div class="flip-card-back">
+                    <h1>Computer Security</h1> 
+                    <p>Teacher: Mr. Elon Musk</p> 
+                    <p>Course Starts on: 12 March, 2023</p>
+                    <p>Course Ends on: 12 July, 2023</p>
+                    <p>Course Fees: 4000 BDT</p>
+                    <abbr title="computer security, also called cybersecurity, the protection of computer systems and information from harm, theft, and unauthorized use. Computer hardware is typically protected by the same means used to protect other valuable or sensitive equipment—namely, serial numbers, doors and locks, and alarms. The protection of information and system access, on the other hand, is achieved through other tactics, some of them quite complex.">Details</abbr>
+                </div>
+            </div>
+        </div>
+        <div class="flip-card">
+            <div class="flip-card-inner">
+                <div class="flip-card-front">
+                    <img src="/coursereg/media/images/sample1.png" class="flip-card-img">
+                </div>
+                <div class="flip-card-back">
+                    <h1>Computer Security</h1> 
+                    <p>Teacher: Mr. Elon Musk</p> 
+                    <p>Course Starts on: 12 March, 2023</p>
+                    <p>Course Ends on: 12 July, 2023</p>
+                    <p>Course Fees: 4000 BDT</p>
+                    <abbr title="computer security, also called cybersecurity, the protection of computer systems and information from harm, theft, and unauthorized use. Computer hardware is typically protected by the same means used to protect other valuable or sensitive equipment—namely, serial numbers, doors and locks, and alarms. The protection of information and system access, on the other hand, is achieved through other tactics, some of them quite complex.">Details</abbr>
+                </div>
+            </div>
+        </div>
+</div>
+</div>
+
+<?php
+    include "../includes/footer.php";
+?>
